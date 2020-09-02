84308fda12f58963584e97d45f0343afd833129b
Dhruv Kansal <dkansal1999@gmail.com>
2020-09-02 20:17:44 +0530

    Updates

---
+++ precache-manifest.1e9f672e07750a47bb6f8d6ac6234d76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3958f009c417dc6f320ffc463cf6de56",
+    "url": "/sort-vis/index.html"
+  },
+  {
+    "revision": "ac6a37ece4687661ced6",
+    "url": "/sort-vis/static/css/main.4508908b.chunk.css"
+  },
+  {
+    "revision": "07ea4eb2dcdf528a5f94",
+    "url": "/sort-vis/static/js/2.cec01007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sort-vis/static/js/2.cec01007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6a37ece4687661ced6",
+    "url": "/sort-vis/static/js/main.f4e20bb9.chunk.js"
+  },
+  {
+    "revision": "ee3ba60e3a99de5ae9f8",
+    "url": "/sort-vis/static/js/runtime-main.92409baa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sort-vis/static/media/logo.5d5d9eef.svg"
+  }
+]);
